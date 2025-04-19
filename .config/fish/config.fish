3f917f07901313e9726227a87a895a299ce34e6a
martindios <gonzalezdiosmartin@gmail.com>
2025-04-19 21:08:15 +0200

    fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,23 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+if test -z "$DISPLAY"; and test (tty) = "/dev/tty1"
+    startx
+end
+
+set fish_greeting ""
+set -x EZA_ICONS_AUTO 1
+
+function ls
+    eza --hyperlink --git-repos $argv
+end
+
+function tree
+    eza --tree --hyperlink --git-repos $argv
+end
+
+function cat
+    bat $argv
+end
+
